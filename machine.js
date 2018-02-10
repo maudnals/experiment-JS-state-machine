83dd148449670c08c9f6f4d919efa316512c5b61
maudnals <maud.nalpas@gmail.com>
2018-02-10 16:05:44 +0100

    added helper methods

--- machine.js
+++ machine.js
@@ -1,18 +1,37 @@
 const machine = {
-  // states as objects. their possible inputs as methods
-  'idle': {
-    click: function() {
-      // click
-    }
-  },
-  'fetching': {
-    success: function() {
+  // initial state
+  // -----------
+  state: 'idle',
+  // transitions
+  // -----------
+  transitions: {
+  // states as objects
+  // possible inputs (transitions) as methods
+  // state names just as namespaces
+    'idle': {
+      click: function () {
+        // click
+      }
     },
-    failure: function() {
+    'fetching': {
+      success: function () {
+      },
+      failure: function () {
+      }
+    },
+    'failed': {
+      retry: function () {
+      }
     }
   },
-  'failed': {
-    retry: function() {
-    }
+  // helper methods
+  // -----------
+  changeStateTo: function(newState) {
+    this.state = newState;
+  },
+  dispatch: function(actionName) {
+    // action as a function
+    const action = this.transitions[this.state][actionName];
   }
+
 }
